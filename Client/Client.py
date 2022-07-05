e50a81041e18426422c41f204219ed062f1f3294
Seth <sethfaulkner95@gmail.com>
2022-07-05 10:46:24 -0400

    Initial Commit

--- Client/Client.py
+++ Client/Client.py
@@ -1,1 +1,12 @@
 
+import socket
+
+while(input("Send Message? (y/n)").lower() != 'n'):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    print(local_ip)
+    sock.connect((local_ip, 11000))
+    sock.send(b"Hello There!<EOF>")
+    sock.recv(4096)
+    sock.close()
